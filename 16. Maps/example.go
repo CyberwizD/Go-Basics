8355ee9d77e31eaaa188b12120f3e9320e95344b
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-03-05 23:19:42 +0100

    add: enhance example code for Maps in Go with additional comments and operations

--- 16. Maps/example.go
+++ 16. Maps/example.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-
+	// Create a map
 	m := make(map[string]int)
 
+	// Assign key-value pair
 	m["k1"] = 7
 	m["k2"] = 13
 
@@ -22,9 +23,11 @@ func main() {
 
 	fmt.Println("len:", len(m))
 
+	// Delete a key from the map
 	delete(m, "k2")
 	fmt.Println("map:", m)
 
+	// Clear the map
 	clear(m)
 	fmt.Println("map:", m)
 
@@ -36,6 +39,7 @@ func main() {
 
 	n2 := map[string]int{"foo": 1, "bar": 2}
 
+	// Check if 'n' map is equal to 'n2' map
 	if maps.Equal(n, n2) {
 		fmt.Println("n == n2")
 	}
